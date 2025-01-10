3db43d6526e34f246281d81915bb195bb44e0ba9
Christophe Hartmann <chartmann.35@gmail.com>
2025-01-10 13:40:46 +0100

    new homelab

---
+++ error.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+</script>
+
+<template>
+  <div>
+    <h1>{{ error.statusCode }}</h1>
+    <p>{{ error.statusMessage }}</p>
+    <p>{{ error.data }}</p>
+    <p>{{ error.cause }}</p>
+    <NuxtLink to="/">Go back home</NuxtLink>
+  </div>
+</template>
