934ae58ad9fffca6179dc3b891531447c9dbabc9
Christophe Hartmann <chartmann.35@gmail.com>
2025-01-10 11:48:05 +0100

    new homelab

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,13 +3,13 @@ import * as Sentry from "@sentry/nuxt"
 const config = useRuntimeConfig()
 
 if (config.public.SENTRY_DSN && typeof config.public.SENTRY_DSN == 'string') {
-  Sentry.init({
-    // If set up, you can use the Nuxt runtime config here
-    // dsn: useRuntimeConfig().public.sentry.dsn, // modify, depending on your custom runtime config
-    dsn: config.public.SENTRY_DSN,
+  // Sentry.init({
+  //   // If set up, you can use the Nuxt runtime config here
+  //   // dsn: useRuntimeConfig().public.sentry.dsn, // modify, depending on your custom runtime config
+  //   dsn: config.public.SENTRY_DSN,
 
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
-    tracesSampleRate: 1.0,
-  })
+  //   // We recommend adjusting this value in production, or using tracesSampler
+  //   // for finer control
+  //   tracesSampleRate: 1.0,
+  // })
 }
